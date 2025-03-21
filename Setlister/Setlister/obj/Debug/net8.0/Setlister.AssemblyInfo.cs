e0b674aa6fc8accc2d1b0c4d5a02413f03ffcf0a
Dec <declanmcglinn@gmail.com>
2025-03-21 12:20:30 +0000

    Updated

--- Setlister/Setlister/obj/Debug/net8.0/Setlister.AssemblyInfo.cs
+++ Setlister/Setlister/obj/Debug/net8.0/Setlister.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Setlister")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f742e022fb944d52703ecbc03ffbee42cce9fc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Setlister")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Setlister")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
